80680516a8e06633950ecabb07a17128f1fc2148
Sayed Hamid Fatimi <hamid@fatimi.co.uk>
2024-02-04 06:12:54 +0000

    move game fetch api to custom hook

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,10 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding='10px'>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
       <ColorModeSwitch />
     </HStack>
