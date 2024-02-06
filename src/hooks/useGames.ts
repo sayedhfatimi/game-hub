12b4f5edddb6b1d38dea2cc86befe89ae3623197
Sayed Hamid Fatimi <hamid@fatimi.co.uk>
2024-02-06 11:23:43 +0000

    smoosh more bugs

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -30,6 +30,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
+    initialPageParam: 1,
     staleTime: 24 * 60 * 60 * 1000, //24h
   });
 
