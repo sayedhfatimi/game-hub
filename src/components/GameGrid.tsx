90fc7ed274530b29118c6ad46d401813b17f7adc
Sayed Hamid Fatimi <hamid@fatimi.co.uk>
2024-02-06 13:48:26 +0000

    refactor interfaces

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
 import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
-import useGames, { Game } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
