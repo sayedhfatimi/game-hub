f5e5ce629a8f95af6108bc229b140d6704dd09a3
Sayed Hamid Fatimi <hamid@fatimi.co.uk>
2024-02-06 11:04:39 +0000

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
-import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
-import { Platform } from "./hooks/usePlatforms";
 
 export interface GameQuery {
   genreId?: number;
